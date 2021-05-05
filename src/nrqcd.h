674e22f34a323febada5c086e593d40f7cb21846
Alba Soto Ontoso <albasotoontoso@Albas-MacBook-Pro.local>
2021-05-05 17:55:48 +0200

    Corrected typos

--- src/nrqcd.h
+++ src/nrqcd.h
@@ -14,8 +14,16 @@ namespace NRQCD{
     double singlet(const double p, const double phip, const double k1, const double phik1,
                    const double kprime, const double phikprime, const double k, const double phik, 
                    const double m);
+    
+    double octet_s10(const double p, const double phip, const double k1, const double phik1,
+                 const double kprime, const double phikprime, const double k, const double phik, 
+                 const double m);
 
-    double octet(const double p, const double phip, const double k1, const double phik1,
+    double octet_s13(const double p, const double phip, const double k1, const double phik1,
+                 const double kprime, const double phikprime, const double k, const double phik, 
+                 const double m);
+
+    double octet_p3j(const double p, const double phip, const double k1, const double phik1,
                  const double kprime, const double phikprime, const double k, const double phik, 
                  const double m);              
 
