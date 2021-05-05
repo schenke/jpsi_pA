c7a99851e1f10aa7b0e7cc41fc490ade3e8626d3
Alba Soto Ontoso <albasotoontoso@Albas-MacBook-Pro.local>
2021-05-05 14:56:01 +0200

    Nrqcd hard elements

---
+++ src/nrqcd.h
@@ -0,0 +1,25 @@
+#ifndef NRQCD_H_
+#define NRQCD_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <sstream>
+#include <fstream>
+#include <string>
+#include <memory>
+#include <iostream>
+
+namespace NRQCD{
+    double singlet(const double p, const double phip, const double k1, const double phik1,
+                   const double kprime, const double phikprime, const double k, const double phik, 
+                   const double m);
+
+    double octet(const double p, const double phip, const double k1, const double phik1,
+                 const double kprime, const double phikprime, const double k, const double phik, 
+                 const double m);              
+
+}
+
+
+#endif // NRQCD_H_
